df3f8c2255b92b7ce0a8f60b5fa350ef260a9f9d
StettenFessler <stetten.fessler@gmail.com>
2021-11-22 16:49:46 -0800

    first commit

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+// run this file separately from the node app anytime want to see the database
+
+const mongoose = require('mongoose');
+const Campground = require('../models/campground'); // include campground schema
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+// get random value in array based off array length
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    // delete everything in the database
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() *  1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+// seedDB returns a promise because it is an asnyc func
+seedDB().then(() => {
+    mongoose.connection.close(); // close database connection
+})
